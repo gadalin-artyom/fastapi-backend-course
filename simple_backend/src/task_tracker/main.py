09a1ce787fe97a017898e90974c2b8af23deacb6
Artyom Gadalin <gadalin.artyom@gmail.com>
2025-03-07 15:17:14 +0300

    change main.py

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,19 +1,68 @@
+import os
+
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+from json_storage import JsonStorage
+from jsonbin import JsonBinStorage
+
+
+load_dotenv()
 
 app = FastAPI()
 
-@app.get("/tasks")
+API_KEY = os.getenv('API_KEY')
+BIN_ID = os.getenv('BIN_ID')
+
+# TaskTracker через ООП
+
+
+class Task(BaseModel):
+    name_task: str
+    status: str
+
+
+class TaskTracker:
+    def __init__(self, storage):
+        self.storage = storage
+
+    def get_tasks(self):
+        return self.storage.read_data()
+
+    def create_task(self, task: Task):
+        self.storage.write_data(task)
+
+    def delete_task(self, task_id):
+        self.storage.delete_data(task_id)
+
+    def change_task(self, task_id, new_info):
+        self.storage.update_data(task_id, new_info)
+
+
+# json_storage = JsonStorage()
+# tracker = TaskTracker(json_storage)
+
+
+jsonbin_storage= JsonBinStorage(API_KEY, BIN_ID)
+tracker = TaskTracker(jsonbin_storage)
+
+
+@app.get('/tasks')
 def get_tasks():
-    pass
+    return tracker.get_tasks()
 
-@app.post("/tasks")
-def create_task(task):
-    pass
+@app.post('/tasks', response_model=Task)
+def create_task(task: Task):
+    tracker.create_task(task)
+    return task
 
-@app.put("/tasks/{task_id}")
-def update_task(task_id: int):
-    pass
+@app.put('/tasks/{task_id}')
+def update_task(task_id: int, new_info: Task):
+    tracker.change_task(task_id, new_info)
+    return new_info
 
-@app.delete("/tasks/{task_id}")
+@app.delete('/tasks/{task_id}')
 def delete_task(task_id: int):
-    pass
+    tracker.delete_task(task_id)
+    return {'status': 'Задача удалена'}
