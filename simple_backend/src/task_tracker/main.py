ef6164a8b3525e1b70806b0de45f3a3c3a86dafb
Artyom Gadalin <gadalin.artyom@gmail.com>
2025-03-07 17:29:07 +0300

    added llm model to tasker

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 
 from json_storage import JsonStorage
 from jsonbin import JsonBinStorage
-
+from cloudflare import CloudflareAI
 
 load_dotenv()
 
@@ -24,14 +24,20 @@ class Task(BaseModel):
 
 
 class TaskTracker:
-    def __init__(self, storage):
+    def __init__(self, storage, ai):
         self.storage = storage
+        self.ai = ai
 
     def get_tasks(self):
         return self.storage.read_data()
 
     def create_task(self, task: Task):
-        self.storage.write_data(task)
+        ai_solution = self.ai.get_solution(task.name_task)
+        task_with_solution = Task(
+            name_task=f"{task.name_task}\n\n Решение:\n{ai_solution}",
+            status=task.status
+        )
+        self.storage.write_data(task_with_solution)
 
     def delete_task(self, task_id):
         self.storage.delete_data(task_id)
@@ -44,8 +50,17 @@ class TaskTracker:
 # tracker = TaskTracker(json_storage)
 
 
+# jsonbin_storage= JsonBinStorage(API_KEY, BIN_ID)
+# tracker = TaskTracker(jsonbin_storage)
+
+
+account_id_CF = os.getenv("ACCOUNT_ID_CLOUDFLARE")
+api_token_CF = os.getenv("API_TOKEN_CLOUDFLARE")
+model_CF = "@cf/meta/llama-3.1-8b-instruct"
+cloudflare = CloudflareAI(account_id_CF, api_token_CF, model_CF)
+
 jsonbin_storage= JsonBinStorage(API_KEY, BIN_ID)
-tracker = TaskTracker(jsonbin_storage)
+tracker = TaskTracker(jsonbin_storage, cloudflare)
 
 
 @app.get('/tasks')
